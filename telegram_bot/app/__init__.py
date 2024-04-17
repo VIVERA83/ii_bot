492486428fb14ef008523ed04e4ab9a1d8d11b2d
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-04-17 23:47:15 +0300

    main app

--- telegram_bot/app/__init__.py
+++ telegram_bot/app/__init__.py
@@ -1,17 +1,14 @@
 import asyncio
 import json
 import logging
-# import uuid
-import re
+
 import uuid
 
-from typing import MutableMapping, Callable, Any, Coroutine
+from typing import MutableMapping, Callable
 
 from aio_pika.abc import AbstractIncomingMessage
-from icecream import ic
-from telethon.events import NewMessage
 
-# from telethon.events import NewMessage
+from telethon.events import NewMessage
 
 from .utils import create_message, COURSE_TYPE, create_future
 from bot.accessor import TgBotAccessor
@@ -49,29 +46,10 @@ class BaseApp:
         self.rabbit = rabbit
         self.logger = logger
         self.callback_queue = None
-        self.in_queue = None
 
         self.futures: MutableMapping[str, asyncio.Future] = {}
         self.users: MutableMapping[str, str] = {}
         self.queue_names: MutableMapping[str, str] = {}
-
-        # self.queue_name = None
-
-    # def bot_decorator(self: Callable):
-    #     async def wrapper(cls: "BaseApp", *args, event: NewMessage.Event, **kwargs):
-    #         correlation_id = uuid.uuid4().hex
-    #         cls.users[correlation_id] = event.message.sender.username
-    #         cls.futures[correlation_id] = create_future()
-    #         result = await self(cls, *args, event=event, **kwargs)
-    #         await cls.rabbit.publish(
-    #             routing_key=cls.routing_key,
-    #             correlation_id=correlation_id,
-    #             reply_to=cls.queue_name,
-    #             body=json.dumps(result).encode("utf-8"),
-    #         )
-    #         return "Запрос принят, ожидается ответ"
-    #
-    #     return wrapper
 
     async def setup_bot_commands(self):
         """
@@ -84,21 +62,7 @@ class BaseApp:
 
     async def start(self):
         await self.setup_bot_commands()
-        # добавляем команды для бота
-        # await self.bot.add_commands([("test", "тестовая команда", self._test_command)])  # noqa
-        # self.bot.update_regex_command_handler(self.create_report_regex_command())
-
-        # подписываемся на получение ответов, и назначаем функцию обратного вызова
-        # self.queue_names[self.routing_key] = await self.rabbit.consume(callback=self._on_response, no_ack=True)
-
-        self.logger.info("Application started")
-        queue = asyncio.Queue(1)
-        while True:
-            try:
-                await queue.get()
-            except asyncio.CancelledError:
-                self.logger.warning("Application cancelled")
-                break
+        await self.__worker()
 
     async def stop(self):
         self.logger.info("Application stopped")
@@ -127,50 +91,12 @@ class BaseApp:
         result = await future
         return result
 
-    def create_report_regex_command(
-            self,
-    ) -> dict[re.Pattern, Callable[[Any], Coroutine[None, None, None]]]:
-        """Create a report regex command.
-
-        Returns:
-            bytes: A dictionary mapping compiled regex patterns to callback functions.
-        """
-        pattern = "[a-zA-Z0-9_]+"
-        return {  # noqa
-            re.compile(f"/report {pattern} {pattern} {pattern}"): self._command
-        }
-
-    async def _test_command(
-            self,
-            *_,
-            **__,
-    ):
-        return "Тестовый запрос принят"
-
-    # @bot_decorator  # noqa
-    # async def _command(
-    #         self,
-    #         login: str,
-    #         password: str,
-    #         course_type: COURSE_TYPE,
-    #         event: NewMessage.Event,
-    #         *_,
-    #         **__,
-    # ) -> Any:
-    #
-    #     data = {"login": login, "password": password, "course_type": course_type}
-    #     # сохраняем идентификатор запроса
-    #     # correlation_id = uuid.uuid4().hex
-    #     # self.users[correlation_id] = event.message.sender.username
-    #     # self.futures[correlation_id] = create_future()
-    #
-    #     # отправляем запрос
-    #     # await self.rabbit.publish(
-    #     #     routing_key=self.routing_key,
-    #     #     correlation_id=correlation_id,
-    #     #     reply_to=self.queue_name,
-    #     #     body=json.dumps(data).encode("utf-8"),
-    #     # )
-    #     self.logger.debug("Incoming test command")
-    #     return data
-    #     # return "Запрос принят, ожидается ответ"
+    async def __worker(self):
+        queue = asyncio.Queue(1)
+        self.logger.info("Application started")
+        while True:
+            try:
+                await queue.get()
+            except asyncio.CancelledError:
+                self.logger.warning("Application cancelled")
+                break
--- telegram_bot/app/magnum.py
+++ telegram_bot/app/magnum.py
@@ -1,3 +1,6 @@
+import re
+from typing import Callable, Any, Coroutine
+
 from telethon.events import NewMessage
 
 from app import BaseApp, COURSE_TYPE, bot_d
@@ -13,3 +16,23 @@ class Magnum(BaseApp):
     async def _command(self, login: str, password: str, course_type: COURSE_TYPE, *_, event: NewMessage.Event):
         data = {"login": login, "password": password, "course_type": course_type}
         return data
+
+    def create_report_regex_command(
+            self,
+    ) -> dict[re.Pattern, Callable[[Any], Coroutine[None, None, None]]]:
+        """Create a report regex command.
+
+        Returns:
+            bytes: A dictionary mapping compiled regex patterns to callback functions.
+        """
+        pattern = "[a-zA-Z0-9_]+"
+        return {  # noqa
+            re.compile(f"/report {pattern} {pattern} {pattern}"): self._command
+        }
+
+    async def _test_command(
+            self,
+            *_,
+            **__,
+    ):
+        return "Тестовый запрос принят"
